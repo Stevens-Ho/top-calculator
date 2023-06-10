d5f0f62ea2915ae1b443e28995300ef01ba33be0
loveprogrammming <106758997+loveprogrammming@users.noreply.github.com>
2023-06-10 11:00:28 +0800

    Display the answer when press '=' button

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function operate(firstNumber, operator, secondNumber) {
         return add(firstNumber, secondNumber);
     } else if (operator === "-") {
         return subtract(firstNumber, secondNumber);
-    } else if (operator === "*") {
+    } else if (operator === "x") {
         return multiply(firstNumber, secondNumber);
     } else if (operator === "÷") {
         return divide(firstNumber, secondNumber);
@@ -29,7 +29,6 @@ function operate(firstNumber, operator, secondNumber) {
 
 function pressNumberBtn() {
     const numberBtn = document.querySelectorAll(".numberBtn");
-
     numberBtn.forEach(number => {
         number.addEventListener("click", function() {
             answerDisplay.textContent += number.textContent;
@@ -37,6 +36,40 @@ function pressNumberBtn() {
     });
 }
 
+function pressMathOperatorBtn() {
+    const mathOperatorBtn = document.querySelectorAll(".mathOperatorBtn");
+    mathOperatorBtn.forEach(btn => {
+        btn.addEventListener("click", function() {
+            if (answerDisplay.textContent) {
+                answerDisplay.textContent += " " + btn.textContent + " ";
+            }
+        });
+    });
+}
+
+function pressEqualBtn() {
+    const equalBtn = document.getElementById("equal");
+    equalBtn.addEventListener("click", function() {
+        let split = answerDisplay.textContent.split(" ");
+        let answer;
+        let iteration = split.length - 2;
+        for (let i = 0; i < iteration; i += 2) {
+            const firstNumber = +split[0+i];
+            const operator = split[1+i];
+            const secondNumber = +split[2+i];
+            if (i === 0) answer = firstNumber;
+            console.log(firstNumber + operator + secondNumber);
+            answer = operate(answer, operator, secondNumber);
+            
+        }
+        answerDisplay.textContent = answer;
+    });
+}
+
 function startCalculator() {
     pressNumberBtn();
+    pressMathOperatorBtn();
+    pressEqualBtn();
 }
+
+startCalculator();
