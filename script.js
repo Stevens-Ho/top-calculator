8dd8366d14f09145c6ae385ba8321dc16aaf8152
loveprogrammming <106758997+loveprogrammming@users.noreply.github.com>
2023-06-14 22:20:07 +0800

    Simplify the code

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const answerDisplay = document.querySelector(".answerDisplay");
 
-function operate(firstNumber, operator, secondNumber) {
-    switch (operator) {
-        case "+":
-            return firstNumber + secondNumber;
-        case "-":
-            return firstNumber - secondNumber;
-        case "x":
-            return firstNumber * secondNumber;
-        case "÷":
-            return firstNumber / secondNumber;
+function operate() {
+    const split = answerDisplay.textContent.split(" ").filter(e => e);
+    const firstNumber = +split[0];
+    const operator = split[1];
+    const secondNumber = +split[2];
+    if (secondNumber) {
+        switch (operator) {
+            case "+":
+                answerDisplay.textContent = firstNumber + secondNumber;
+                break;
+            case "-":
+                answerDisplay.textContent = firstNumber - secondNumber;
+                break;
+            case "x":
+                answerDisplay.textContent = firstNumber * secondNumber;
+                break;
+            case "÷":
+                answerDisplay.textContent = firstNumber / secondNumber;
+                break;
+        }
     }
 }
 
@@ -20,7 +30,6 @@ function pressNumberBtn() {
             if (number.textContent === keyboard.key) addEventListenerNumberBtn(number);
         });
         number.addEventListener("click", () => addEventListenerNumberBtn(number));
-        
     });
 }
 
@@ -62,57 +71,37 @@ function pressMathOperatorBtn() {
     });
 }
 
-function calculateSolution() {
-    const split = answerDisplay.textContent.split(" ").filter(e => e);  
-    let answer;
-    if (split.length > 2) {
-        const iteration = split.length - 2;
-        for (let i = 0; i < iteration; i += 2) {
-            const firstNumber = +split[0+i];
-            const operator = split[1+i];
-            const secondNumber = +split[2+i];
-            if (i === 0) answer = firstNumber;
-            answer = operate(answer, operator, secondNumber);
-        }
-        return answerDisplay.textContent = answer;
-    }
-    return answerDisplay.textContent;
-}
-
 function addEventListenerMathOperator(btn) {
     const mathOperator = " " + btn.textContent + " ";
     if (!/ [\+\-x÷] /g.test(answerDisplay.textContent) && answerDisplay.textContent) {
         answerDisplay.textContent += mathOperator;
     } else if (/ [\+\-x÷] $/g.test(answerDisplay.textContent)) {
-        answerDisplay.textContent.replace(" + ", mathOperator);
         answerDisplay.textContent = answerDisplay.textContent.replace(/ [\+\-x÷] /g, mathOperator);
     }  else if (/ [\+\-x÷] /g.test(answerDisplay.textContent)) {
-        calculateSolution();
+        operate();
         answerDisplay.textContent += mathOperator;
     }
 }
 
 function pressEqualBtn() {
     const equalBtn = document.getElementById("equal");
-    equalBtn.addEventListener("click", calculateSolution);
+    equalBtn.addEventListener("click", operate);
     window.addEventListener("keypress", keyboard => {
         if (keyboard.key === "=" || keyboard.key === "Enter") {
-            calculateSolution();
+            operate();
         }
     });
 }
 
 function pressClearBtn() {
     const clear = document.getElementById("clear");
-    clear.addEventListener("click", function() {
-        answerDisplay.textContent = "";
-    });
+    clear.addEventListener("click", () => answerDisplay.textContent = "");
 }
 
 function pressDeleteBtn() {
     const del = document.getElementById("del");
     del.addEventListener("click", () => {
-        if (answerDisplay.textContent[answerDisplay.textContent.length - 1] === " ") {
+        if (/ [\+\-x÷] $/g.test(answerDisplay.textContent)) {
             answerDisplay.textContent = answerDisplay.textContent.slice(0, -3);
         } else {
             answerDisplay.textContent = answerDisplay.textContent.slice(0, -1);
@@ -127,7 +116,7 @@ function addEventListenerDot() {
     const secondNumber = split[2];
     if ((!/\./.test(firstNumber) && /\d+/.test(firstNumber) && !operator) ||
     (!/\./.test(secondNumber) && /\d+/.test(secondNumber))) {
-        return answerDisplay.textContent += dot.textContent;
+        answerDisplay.textContent += dot.textContent;
     }
 }
 
